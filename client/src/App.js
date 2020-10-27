d8525cbc188df308764c9730441876409719613f
matthanson333 <matthanson333@gmail.com>
2020-10-26 21:03:32 -0400

    migrated geolocator component to new repo

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,9 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
-import Spotify from './pages/Spotify';
+import Spotify from "./pages/Spotify";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-
 
 // represents the main control for the html displays
 function App() {
